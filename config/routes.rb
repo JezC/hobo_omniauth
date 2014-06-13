deb049a500fd94228c087a993845ee51579da47d
Jeremy Chatfield <jezchatfield@gmail.com>
2014-06-13 16:06:40 +0100

    retry the whole match thing again
    This time, read the Rails 4 docs. Still haven’t looked at how this
    works in Omniauth, yet. That’ll come, I’m sure.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   @controller = ApplicationController.subclasses.find{|c| c.ancestors.include?(HoboOmniauth::Controller)}
   if @controller
-    via: [:get, :post] '/auth/:provider/callback' => "#{@controller.controller_name}#omniauth_callback"
-    via: [:get, :post] '/auth/failure' => "#{@controller.controller_name}#omniauth_callback"
+    match '/auth/:provider/callback' => "#{@controller.controller_name}#omniauth_callback", via: [:get, :post]
+    match '/auth/failure' => "#{@controller.controller_name}#omniauth_callback", via: [:get, :post]
   end
 end
